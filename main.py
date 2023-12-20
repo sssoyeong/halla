81e30b8fb877310b09419ed4fd0cc43553b78470
sssoyeong <sypark.for.work@gmail.com>
2023-12-20 18:00:23 +0900

    ~ing

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+import time
+import re
+
+from playsound import playsound
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from bs4 import BeautifulSoup
+
+
+# 브라우저 꺼짐 방지 옵션
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+if sys.platform == 'darwin':
+    chrome_options.add_argument('--start-maximized')
+
+driver = webdriver.Chrome(options=chrome_options)
+
+url_home = 'https://visithalla.jeju.go.kr/reservation/status.do'
+driver.get(url_home)
+
+while True:
+    count_26 = int(driver.find_element(By.XPATH, '//*[@id="TD_20231226"]/a/div/div/span').text)
+    count_27 = int(driver.find_element(By.XPATH, '//*[@id="TD_20231227"]/a/div/div/span').text)
+    if (count_26 < 2) | (count_27 < 2):
+        driver.refresh()
+        driver.implicitly_wait(10)
+    else:
+        playsound("alertsound.mp3")
+
