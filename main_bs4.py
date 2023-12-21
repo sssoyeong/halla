de53137d998303df06477f40c320cbbccf61c7b1
sssoyeong <sypark.for.work@gmail.com>
2023-12-21 17:33:55 +0900

    error correction

--- main_bs4.py
+++ main_bs4.py
@@ -8,7 +8,11 @@ import requests
 # from playsound import playsound
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url = 'https://visithalla.jeju.go.kr/reservation/status.do'
@@ -20,14 +24,13 @@ while True:
         soup = BeautifulSoup(html, 'html.parser')
         count_26 = int(soup.select_one('#TD_20231226 > a > div > div > span').get_text(strip=True))
         count_27 = int(soup.select_one('#TD_20231227 > a > div > div > span').get_text(strip=True))
-        if (count_26 < 2) | (count_27 < 2):
+        if (count_26 < 2) & (count_27 < 2):
             print(f'{time.strftime("[%y%m%d %H:%M:%S]")} : not found')
         else:
             print(f'{time.strftime("[%y%m%d %H:%M:%S]")} : F O U N D ')
-            chrome_options = Options()
-            chrome_options.add_experimental_option("detach", True)
-            if sys.platform == 'darwin':
-                chrome_options.add_argument('--start-maximized')
+            print(f'26일: {count_26}, 27일: {count_27}')
+            print(url)
+
             driver = webdriver.Chrome(options=chrome_options)
             driver.get(url)
             break
